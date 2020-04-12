2d826061b92417091063c051a2fff885b23166ae
Iza Witkowska <isa.witkowska@gmail.com>
2020-04-12 19:06:17 +0200

    Modify const

--- js/script.js
+++ js/script.js
@@ -1,47 +1,44 @@
-{
+{    
+const getMoveName = function(argMoveId) {
+    if(argMoveId === 1) {
+    return 'kamień';
+    } 
+    if(argMoveId === 2) {
+    return 'papier';
+    } 
+    if(argMoveId === 3) {
+    return 'nożyce';
+    } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+    }
+}    
+const displayResult = function(argComputerMove,argPlayerMove) {
+    console.log('moves:', argComputerMove, argPlayerMove)
+    if(argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+    }
+    if(argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+    }
+    if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+    }
+    if(argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+    printMessage('Przegrywasz!');
+    }
+    if(argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+    printMessage('Przegrywasz!');
+    }
+    if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+    printMessage('Przegrywasz!');
+    }
+    if(argComputerMove == argPlayerMove) {
+    printMessage('Remis!')
+    }
+}
 const playGame = function(argPlayerInput) {
-//function playGame(argPlayerInput) {
     clearMessages();
-    const getMoveName = function(argMoveId){
-    //function getMoveName(argMoveId) {
-        if(argMoveId === 1) {
-        return 'kamień';
-        } 
-        if(argMoveId === 2) {
-        return 'papier';
-        } 
-        if(argMoveId === 3) {
-        return 'nożyce';
-        } else {
-        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-        return 'nieznany ruch';
-        }
-    }
-    const displayResult = function(argComputerMove,argPlayerMove) {
-    //function displayResult(argComputerMove,argPlayerMove) {
-        console.log('moves:', argComputerMove, argPlayerMove)
-        if(argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-        printMessage('Ty wygrywasz!');
-        }
-        if(argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-        printMessage('Ty wygrywasz!');
-        }
-        if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-        printMessage('Ty wygrywasz!');
-        }
-        if(argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-        printMessage('Przegrywasz!');
-        }
-        if(argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-        printMessage('Przegrywasz!');
-        }
-        if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
-        printMessage('Przegrywasz!');
-        }
-        if(argComputerMove == argPlayerMove) {
-        printMessage('Remis!')
-        }
-    }
 
     const randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('Wylosowana liczba to: ' + randomNumber);
